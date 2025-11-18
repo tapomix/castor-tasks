00deee8956b4e1bba3a56341d9cf924d795835ec
Tapomix <dev@tapomix.me>
2025-11-18 12:17:00 +0100

    [code] load app env file

--- src/context.php
+++ src/context.php
@@ -14,6 +14,14 @@ define('TAPOMIX_DEFAULT_CONTEXT', 'tapomix-default');
 #[AsContext(name: TAPOMIX_DEFAULT_CONTEXT)] // don't defined as default to allow override
 function default_context(): Context
 {
+    if ( // constant defined in castor.php from app
+        \defined('TAPOMIX_APP_ENV_FILE')
+        && fs()->exists(TAPOMIX_APP_ENV_FILE)
+    ) {
+        // load app env first to enable variable expansion
+        load_dot_env(TAPOMIX_APP_ENV_FILE);
+    }
+
     $castorEnvFile = '.castor/.env.castor';
 
     if (fs()->exists($castorEnvFile)) {
